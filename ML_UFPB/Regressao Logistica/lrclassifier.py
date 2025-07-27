8a0d36f0e5d3fddbd18e794b82ce7d7abf6cf929
Gusttavoafonso <gustavoafonso892@gmail.com>
2025-07-27 10:36:01 -0300

    Feat: Resolução Pratica 1 R_Logist

--- ML_UFPB/Regressao Logistica/lrclassifier.py
+++ ML_UFPB/Regressao Logistica/lrclassifier.py
@@ -1,12 +1,28 @@
-from ..Pratica_RL import LinearRegression
-import numpy
+import numpy as np
 #from pocketpla import PocketPLA
+
+class LinearRegression:
+    def fit(self, _X, _y):
+        _X = np.array(_X)
+        _y = np.array(_y)
+        #_X = np.hstack((_X, np.ones((_X.shape[0], 1)))) caso n estivesse os 1 no timeRL
+        arg1 = _X.T @ _X
+        arg2 = _X.T @ _y
+        inversa = np.linalg.pinv(arg1)
+        self.w = inversa  @  arg2
+     
+    def predict(self, _x):
+    # _x = np.hstack((_x, np.ones((_x.shape[0], 1))))
+     return _x @ self.w
+
+    def getW(self):
+        return self.w
 
 class LRClassifier():
     def fit(self, _X, _y):
         lr = LinearRegression()
-        lr.execute(_X, _y)
-        self.w = lr.get_w()
+        lr.fit(_X, _y)
+        self.w = lr.getW()
         
         #pla = PocketPLA()
         #pla.set_w(self.w)
@@ -15,7 +31,7 @@ class LRClassifier():
         
         
     def predict(self, _X):
-        return [numpy.sign(numpy.dot(self.w, x)) for x in _X]
+        return [np.sign(np.dot(self.w, x)) for x in _X]
      
     def getRegressionY(self, regressionX, shift=0):
         return (-self.w[0]+shift - self.w[1]*regressionX) / self.w[2]
